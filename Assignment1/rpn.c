48ef6ef223673cefa4b27c80839a3e45dd5e45c8
Patrik Brosell <patrikbrosell@gmail.com>
2015-09-08 18:45:22 +0000

    Improved rpn.c

--- Assignment1/rpn.c
+++ Assignment1/rpn.c
@@ -10,83 +10,96 @@ size_t i = 0;
 int line = 1;
 int c;
 
-void flush()
-{
-	while (c != '\n') {
-		c = getchar();
-	}
-	i = 0;
-}
+//void push(int val)
+//{
+//	if (i < 10) {
+//		stack[i++] = val;
+//	}
+//}
+
+//int pop()
+//{
+//	if (i > 0) {
+//		return stack[--i];
+//	} else if (i == 0) {
+//		return stack[i];
+//	}
+//	return -1;
+//}
+
+//void flush()
+//{
+//	while ((c = getchar()) != '\n') {
+//		c = getchar();
+//	}
+//	i = 0;
+//}
 
 void error(char c)
 {
 	printf("line %d: error at %c\n", line++, c);
-}
-
-void push(int val)
-{
-	if (i < 10) {
-		stack[i++] = val;
+	while ((c = getchar()) != '\n') {
 	}
-}
-
-int pop()
-{
-	if (i > 0) {
-		return stack[--i];
-	} else if (i == 0) {
-		return stack[i];
-	}
-	return -1;
+	i = 0;
 }
 
 int main(void)
 {
-	c = getchar();
+//	c = getchar();
 	int tmp = 0;
-	do {
-//		tmp = 0;
+	while ((c = getchar()) != EOF) {
+//	do {
+		tmp = 0;
 		if (isdigit(c)) {
-			tmp = 0;
 			while (isdigit(c)) {
 				tmp = 10 * tmp + c - '0';
 				c = getchar();
 			}
 			if (i >= 10) {
 				error(tmp + '0');
-				flush();
+//				flush();
 				continue;
 			} else {
-				push(tmp);
+				stack[i++] = tmp;
+//				push(tmp);
 			}
-//			tmp = 0;
 		}
 
 		if (!isdigit(c)) {
 			switch (c) {
 			case '*':
-				push(pop() * pop());
+				stack[i-2] = stack[i-2] * stack[i-1];
+				i--;
+//				push(pop() * pop());
 				break;
 			case '/':
-				tmp = pop();
-				if (tmp == 0) {
+//				tmp = pop();
+				if (stack[i-1] == 0) {
+//				if (tmp == 0) {
 					error('/');
-					flush();
+//					flush();
+
 				} else {
-					push(pop() / tmp);
+					stack[i-2] = stack[i-2] / stack[i-1];
+					i--;
+//					push(pop() / tmp);
 				}
 				break;
 			case '+':
 				if (i <= 1) {
 					error('+');
-					flush();
+//					flush();
 				} else {
-					push(pop() + pop());
+					stack[i-2] = stack[i-1] + stack[i-2];
+//					push(pop() + pop());
+					i--;
 				}
 				break;
 			case '-':
-				tmp = pop();
-				push(pop() - tmp);
+//				tmp = pop();
+//				push(pop() - tmp);
+				stack[i-2] = stack[i-2] - stack[i-1];
+				i--;
 				break;
 			case ' ':
 				break;
@@ -94,15 +107,18 @@ int main(void)
 				if (i != 1) {
 					printf("line %d: error at \\n\n", line++);
 				} else {
-					printf("line %d: %d\n", line++, pop());
+//					printf("line %d: %d\n", line++, pop());
+					printf("line %d: %d\n", line++, stack[--i]);
 				}
+				i = 0;
 				break;
 			default:
 				error(c);
-				flush();
+//				flush();
 				break;
 			}
 		}
-	} while ((c=getchar()) != EOF);
+	}
+//	} while ((c=getchar()) != EOF);
 	return 0;
 }
