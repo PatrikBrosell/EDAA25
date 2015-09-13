492baac8aa319511ff49281e54838d4b92a0aa97
Patrik Brosell <patrikbrosell@gmail.com>
2015-09-13 23:06:30 +0000

    Update assignment 2

--- Assignment2/word.c
+++ Assignment2/word.c
@@ -11,35 +11,25 @@ char prnt[] = "%d characters in longest word: %s\n";
 int main(void)
 {
 	char * string;
-	string = malloc(longest_length * sizeof(char) + 10);
-//	string = malloc(max * sizeof(char) + 10);
+	string = malloc(max * sizeof(char) + 10);
 	char * longest;
-	longest = malloc(longest_length * sizeof(char) + 10);
-//	longest = malloc(longest_length * sizeof(char) + 10);
-	do {
-	c = getchar();
+	longest = malloc(max * sizeof(char) + 10);
+	while ((c=getchar()) != EOF) {
 		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-			*(string + length++) = c;
-			if (length >= longest_length) {
-//				printf("inne\n");
-//				max = 2*max;
-				longest_length = length;
-//				printf("%d\n", longest_length);
-//				printf("%d\n", length);
-				string = realloc(string, 100 + longest_length * sizeof(char));
-				longest = realloc(longest, 100 + longest_length * sizeof(char));
-				strcpy(longest, string);
+			if (length >= max) {
+				max = 2*max;
+				string = realloc(string, max * sizeof(char));
+				longest = realloc(longest, max * sizeof(char));
 			}
+			*(string + length++) = c;
 			continue;
-		} else {
-//		else if (length >= longest_length) {
-//			longest_length = length;
-//			strcpy(longest, string);
-//		}
-		length = 0;
 		}
-	} while (c != EOF);
-
+		if (length >= longest_length) {
+			longest_length = length;
+			strcpy(longest, string);
+		}
+		length = 0;
+	}
 	free(string);
 	printf(prnt, longest_length, longest);
 	free(longest);
