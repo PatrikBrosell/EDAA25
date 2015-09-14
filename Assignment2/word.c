48dbbc8163bc927a677a00418bb3100e89b75a40
Patrik Brosell <patrikbrosell@gmail.com>
2015-09-14 01:48:52 +0000

    Improved assignment 2

--- Assignment2/word.c
+++ Assignment2/word.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-int length = 0;
-int longest_length = 0;
-int c;
-int max = 10;
 char prnt[] = "%d characters in longest word: %s\n";
 
 int main(void)
 {
-	char * string = malloc(max);
-	char * longest = malloc(max);
+	int length = 0;
+	int longest_length = 0;
+	int c;
+	int max = 10;
+	char *string = malloc(max);
+	char *longest = malloc(max);
+
 	while ((c=getchar()) != EOF) {
 		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
 			*(string + length++) = c;
-			if (length >= max) {
+			if (length == max) {
 				max = 2*max;
 				string = realloc(string, max);
 				longest = realloc(longest, max);
@@ -27,8 +28,8 @@ int main(void)
 		}
 		length = 0;
 	}
-	free(string);
 	printf(prnt, longest_length, longest);
+	free(string);
 	free(longest);
 	return 0;
 }
