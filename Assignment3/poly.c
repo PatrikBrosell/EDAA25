61d7c3a922039618b32e3aa1520a74174d6cbc65
Patrik Brosell <patrikbrosell@gmail.com>
2015-09-14 07:32:17 +0000

    Started on assignment 3

--- Assignment3/poly.c
+++ Assignment3/poly.c
@@ -2,6 +2,151 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "error.h"
 #include "poly.h"
+
+#define MAX 10
+
+
+struct poly_t {
+	int expo[MAX];
+	int coef[MAX];
+};
+
+poly_t* new_poly_from_string(const char* input)
+{
+	poly_t* poly = malloc(sizeof(poly_t*));
+	int last_is_expo = 1;
+	int sign = 1; //1 er plus, -1 er minus
+	int c = 0;
+	int nbr = 0;
+	int ci = 0; //coef index
+	int ei = 0; //expo index
+
+
+
+
+
+
+
+
+
+
+	while ((c = *input++) != '\0') {
+		while(isdigit(c)) {
+			nbr = 10 * nbr + c - '0';
+			c = *input++;
+		}
+
+		if (last_is_expo == 1 && nbr != 0) {
+//			da e de en coeff
+			poly->coef[ci++] = sign*nbr;
+			last_is_expo = 0;
+			nbr = 0;
+			continue;
+		}
+		if (last_is_expo == 1 && c == 'x') {
+//			da e de coef 1
+			poly->coef[ci++] = 1;
+			last_is_expo = 0;
+			nbr = 0;
+			continue;
+		}
+		if (last_is_expo == 0 && nbr != 0) {
+//			da e de exponent
+			poly->expo[ei++] = nbr;
+			last_is_expo = 1;
+			nbr = 0;
+			continue;
+		}
+		if (c == '+') {
+			sign = 1;
+			if (last_is_expo == 0) {
+				poly->expo[ei++] = 1;
+				last_is_expo = 1;
+			}
+		}
+		if (c == '-') {
+			sign = -1;
+			if (last_is_expo == 0) {
+				poly->expo[ei++] = 1;
+				last_is_expo = 1;
+			}
+		}
+
+
+
+
+
+		nbr = 0; //reset nbr at end of loop.
+	}
+	return poly;
+}
+
+void free_poly(poly_t* poly)
+{
+	free(poly);
+}
+
+poly_t* mul(poly_t* a, poly_t* b)
+{
+	return NULL;
+}
+
+void print_poly(poly_t* poly)
+{
+	int tmpcoef = 0;
+	int tmpexpo = 0;
+	for (int i = 0; i < 3; i++) {
+		tmpcoef = poly->coef[i];
+		tmpexpo = poly->expo[i];
+
+		if (tmpcoef < 0) {
+			printf(" - ");
+			tmpcoef = -tmpcoef;
+		} else if (i != 0 && tmpcoef != 0) {
+			printf(" + ");
+		}
+
+		switch (tmpcoef) {
+		case 0:
+			//do nothing
+			break;
+		case 1:
+			if (tmpexpo != 0) {
+//				printf("x");
+			} else {
+				printf("%d", tmpcoef);
+			}
+			break;
+		default:
+			printf("%d", tmpcoef);
+		}
+
+		switch (tmpexpo) {
+		case 0:
+			//do nothing
+			break;
+		case 1:
+			printf("x");
+			break;
+		default:
+			printf("x^%d", tmpexpo);
+
+		}
+	}
+	printf("\n");
+
+
+//	WOHO DET FUNKAR
+//	for (int i = 0; i < 3; i++) {
+//		printf("%d ", poly->coef[i]);
+//	}
+//	printf("\n");
+//	for (int i = 0; i < 3; i++) {
+//		printf("%d ", poly->expo[i]);
+//	}
+//	printf("\n\n");
+}
