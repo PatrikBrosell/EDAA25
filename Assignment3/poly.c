053e0667a8ae6b991a3374b670a6180f983ddb66
Patrik Brosell <patrikbrosell@gmail.com>
2015-09-15 11:56:36 +0200

    Assignment3 works with local test

--- Assignment3/poly.c
+++ Assignment3/poly.c
@@ -7,7 +7,7 @@
 #include "error.h"
 #include "poly.h"
 
-#define MAX 10*sizeof(int)
+#define MAX 10
 
 
 struct poly_t {
@@ -17,16 +17,20 @@ struct poly_t {
 
 poly_t* new_poly_from_string(const char* input)
 {
-	poly_t* poly = malloc(2*sizeof(poly_t*));
+	poly_t* poly = calloc(1, sizeof(poly_t));
 	int last_is_expo = 1;
 	int sign = 1; //1 er plus, -1 er minus
 	int c = 0;
 	int nbr = 0;
 	int ci = 0; //coef index
 	int ei = 0; //expo index
-	while ((c = *input++) != '\0') {
+	int stop = 0;
+	while ((c = *input++) != '\0' && stop == 0) {
 		while(isdigit(c)) {
 			nbr = 10 * nbr + c - '0';
+			if (strlen(input) == 0) {
+				stop = 1;
+			}
 			c = *input++;
 		}
 
@@ -66,6 +70,9 @@ poly_t* new_poly_from_string(const char* input)
 			}
 		}
 		nbr = 0; //reset nbr at end of loop.
+		if (c == '\0') {
+			break;
+		}
 	}
 	return poly;
 }
@@ -77,28 +84,53 @@ void free_poly(poly_t* poly)
 
 poly_t* mul(poly_t* a, poly_t* b)
 {
-	poly_t* r = malloc(2*sizeof(poly_t*));
+	poly_t* r = calloc(100, sizeof(poly_t));
+
+	int coef[MAX*MAX] = { 0 };
+	int expo[MAX*MAX] = { 0 };
 
 	int k = 0;
-	for(int i = 0; i < 3; i++) {
-		for(int j = 0; j < 3; j++) {
-			r->coef[k] = a->coef[i] * b->coef[j];
-			r->expo[k] = a->expo[i] + b->expo[j];
+	for(int i = 0; i < MAX; i++) {
+		for(int j = 0; j < MAX; j++) {
+//			r->coef[k] = a->coef[i] * b->coef[j];
+//			r->expo[k] = a->expo[i] + b->expo[j];
+			coef[k] = a->coef[i] * b->coef[j];
+			expo[k] = a->expo[i] + b->expo[j];
 			k++;
 		}
 	}
 	//sort
-	for(int i = 0; i < 3; i++) {
-		for(int j = 0; j < 3; j++){
-			if (r->expo[i] == r->expo[j] && i != j) {
-				r->coef[i] = r->coef[i] + r->coef[j];
-				r->coef[j] = 0;
-				r->expo[j] = 0;
+	for(int i = 0; i < MAX*MAX; i++) {
+		for(int j = 0; j < MAX*MAX; j++){
+//			if (r->expo[i] == r->expo[j] && i != j) {
+			if (expo[i] == expo[j] && i != j) {
+//				r->coef[i] = r->coef[i] + r->coef[j];
+//				r->coef[j] = 0;
+				coef[i] = coef[i] + coef[j];
+				coef[j] = 0;
+				expo[j] = 0;
 			}
-
-
 		}
 	}
+	int tmp = 0;
+	for (int i = 0; i < MAX*MAX; i++) {
+		if ((coef[i] == 0) && (expo[i] == 0)) {
+			tmp = i;
+			// i e forsta tomma
+			while((coef[tmp++] == 0)) {
+				 if ((tmp < MAX*MAX) &&(coef[tmp] != 0)) {
+					coef[i] = coef[tmp];
+					expo[i] = expo[tmp];
+					coef[tmp] = 0;
+					expo[tmp] = 0;
+					break;
+				}
+			}
+		}
+	}
+
+	memcpy(r->coef, coef, MAX*sizeof(int));
+	memcpy(r->expo, expo, MAX*sizeof(int));
 	return r;
 }
 
@@ -106,7 +138,7 @@ void print_poly(poly_t* poly)
 {
 	int tmpcoef = 0;
 	int tmpexpo = 0;
-	for (int i = 0; i < 1; i++) {
+	for (int i = 0; i < MAX; i++) {
 		tmpcoef = poly->coef[i];
 		tmpexpo = poly->expo[i];
 		if (tmpcoef == 0) {
