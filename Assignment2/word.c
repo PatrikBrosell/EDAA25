80e07c114ca217b154214064a0158485a16843a4
Patrik Brosell <patrikbrosell@gmail.com>
2015-09-17 08:56:27 +0200

    Improved word.c

--- Assignment2/word.c
+++ Assignment2/word.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-
-char prnt[] = "%d characters in longest word: %s\n";
+#include <ctype.h>
 
 int main(void)
 {
-	int length = 0;
-	int longest_length = 0;
+	int longest = 0;
 	int c;
-	int max = 10;
+	int max = 100;
+	int longest_start = 0;
+	int i = 0;
+	int tmp_start = 0;
 	char *string = malloc(max);
-	char *longest = malloc(max);
 
 	while ((c=getchar()) != EOF) {
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-			*(string + length++) = c;
-			if (length == max) {
-				max = 2*max;
-				string = realloc(string, max);
-				longest = realloc(longest, max);
+		if (i >= max) {
+			max = 2 * max;
+			string = realloc(string, max);
+		}
+		if ((c > 'z' || c < 'a') && (c < 'A' || c > 'Z')) {
+			*(string + i) = '\0';
+			if ((i - tmp_start) > longest) {
+				longest = i - tmp_start;
+				longest_start = tmp_start;
 			}
-			continue;
+			tmp_start = ++i;
+		} else {
+			*(string + i++) = c;
 		}
-		if (length >= longest_length) {
-			longest_length = length;
-			strcpy(longest, string);
-		}
-		length = 0;
 	}
-	printf(prnt, longest_length, longest);
+
+	printf("%d characters in longest word: %s\n", longest, string + longest_start);
 	free(string);
-	free(longest);
 	return 0;
 }
